5c67f87e5ff8069f80a6f680920ea1c4a8fb5bb0
ardenbijou <snavelya@wlhs.wlwv.k12.or.us>
2024-10-25 14:37:21 -0700

    eep

---
+++ src/lesson_3_6/Main.java
@@ -0,0 +1,9 @@
+package lesson_3_6;
+
+public class Main {
+    public static void main(String[] args) {
+        dice dice1 = new dice;
+        dice dice2 = new dice;
+        dice dice3 = new dice;
+    }
+}
---
+++ src/lesson_3_6/dice.java
@@ -0,0 +1,9 @@
+package lesson_3_6;
+
+public class dice {
+    private int value;
+
+
+
+    }
+}
